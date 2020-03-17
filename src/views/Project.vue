bd2a9a3e8b1cc50cdab67a4408fab8eaa4940428
RNubla <loopyelbarto@gmail.com>
2020-03-17 17:14:53 -0400

    added project page

---
+++ src/views/Project.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <HelloWorld msg='This is the the project page'>
+    </div>
+</template>
+
+<script>
+import HelloWorld from '@components/HelloWorld.vue'
+export default {
+    name: 'Project',
+    components:{
+        HelloWorld
+    }
+}
+</script>
