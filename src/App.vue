6c4d1f94dfe4734bf4cdcef771e3d23f0c5c7b6e
RNubla <loopyelbarto@gmail.com>
2020-03-17 17:42:14 -0400

    reverted and removed sass style from app

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,25 @@
   </div>
 </template>
 
-<style lang="sass">
-@import '../node_modules/bulma/bulma.sass'
-@import 'mq'
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
