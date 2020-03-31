2ac44cee32d2d88e9bf11e1ab029a9e47a0177a0
RNubla <loopyelbarto@gmail.com>
2020-03-31 15:59:55 -0400

    removed the navbar

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,44 @@
 <template>
-  <div class="columns">
-    <div class="column"></div>
-    <div class="column is-four-fifths">
-      <div id="app">
-        <div id="nav">
-          <Navbar/>
-        </div>
-        <router-view/>
-        <footer class="footer">
-          <div class="content has-text-centered">
-            <Footer/>
-          </div>
-        </footer>
-      </div>
+  <div id="app">
+    <div id="nav">
+      <!-- <Navbar /> -->
     </div>
-    <div class="column"></div>
+    <router-view />
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <Footer />
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar.vue'
-import Footer from '@/components/Footer.vue'
+// import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
 export default {
-  components:{
-    Navbar,
+  components: {
+    // Navbar,
     Footer
   }
-}
+};
 </script>
 <style lang="scss">
 $footer-background-color: white;
 // $title-family: libre baskerville,Georgia,serif;
-@import url('https://fonts.googleapis.com/css?family=Libre+Baskerville:400,700|Nanum+Gothic:400,700&display=swap');
+@import url("https://fonts.googleapis.com/css?family=Libre+Baskerville:400,700|Nanum+Gothic:400,700&display=swap");
 @import "~bulma/css/bulma.css";
 
-#app{
-  font-family: libre baskerville,Georgia,serif;
+#app {
+  font-family: libre baskerville, Georgia, serif;
   -webkit-font-smoothing: antialiased;
-  
 }
-.footer{
+.footer {
   background-color: white;
+}
+html,
+body {
+  // font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+  // font-size: 14px;
+  background: #f0f2f4;
 }
 </style>
